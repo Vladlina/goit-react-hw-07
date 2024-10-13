8b2a6fe0f64f43121044428998719ba2d83f9d7a
Vladlina <92329114+Vladlina@users.noreply.github.com>
2024-10-13 23:28:10 +0200

    app completion

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
 import ContactList from "./components/ContactList/ContactList";
 import SearchBox from "./components/SearchBox/SearchBox";
 import ContactForm from "./components/ContactForm/ContactForm";
+import { fetchContacts } from "./redux/contactsOps";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
     <div>
       <h1 style={{ marginBottom: "12px", color: "darkred" }}>Phonebook</h1>
